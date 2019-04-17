5a7bfde8e32f23343795c5b4986cf5ad5d6ba149
Joli Holmes <jh111@ad.rice.edu>
2019-04-17 15:40:35 -0500

    initial commit

---
+++ R/cli_args.R
@@ -0,0 +1,13 @@
+# Command line Interface Arguments
+
+parseArguments <- function() {
+    #' Parses different options from the command line
+    
+    option_list <- list(optparse::make_option(c("-ab", "--add_branch"), action = "store_true", help = "Add the branch to the commit tag [default= %default]"), 
+        optparse::make_option(c("-am", "--add_message"), action = "store_true", help = "Add the message to the commit tag [default= %default]"), 
+        optparse::make_option(c("-c", "--commit_message"), action = "store", help = "The commit message [default= %default]"))
+    
+    opt_parser <- optparse::OptionParser(option_list = option_list)
+    return(opt_parser)
+    
+}
