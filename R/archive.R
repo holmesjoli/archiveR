4152daac10a6b269ce6f2fc8b751340aff333ed7
Joli Holmes <jh111@ad.rice.edu>
2019-04-18 16:31:15 -0500

    added write_output function

--- R/archive.R
+++ R/archive.R
@@ -31,21 +31,21 @@ create_archive_str <- function(output_dir) {
     
 }
 
-move_to_current <- function(archive_fls, current_dir) {
+move_to_current <- function(fls, current_dir) {
     #' Moves the files to the Current folder
-    #' @param archive_fls a list of the files to archive
+    #' @param fls a list of the files to archive
     #' @param current_dir the current directory
     #' @examples 
     #' output_dir <- '.'
     #' current_dir <- create_current_dir(output_dir)
-    #' archive_fls <- c('data.csv', 'data_log.log')
-    #' utilsR::create_files(archive_fls)
-    #' move_to_current(archive_fls, current_dir)
+    #' fls <- c('data.csv', 'data_log.log')
+    #' utilsR::create_files(fls)
+    #' move_to_current(fls, current_dir)
     
     utilsR::remove_files(dir(current_dir, full.names = T))
     
-    assertthat::assert_that(all(sapply(1:length(archive_fls), FUN = function(x) {
-        file.rename(archive_fls[x], file.path(current_dir, basename(archive_fls[x])))
+    assertthat::assert_that(all(sapply(1:length(fls), FUN = function(x) {
+        file.rename(fls[x], file.path(current_dir, basename(fls[x])))
     })))
 }
 
@@ -58,7 +58,7 @@ create_archive <- function(commit, current_dir, archive_dir) {
     #' commit <- 'master_98jgh8'
     #' output_dir <- '.'
     #' archive_files <- c('data.csv', 'data_log.log')
-    #' create_archive(commit, output_dir, archive_fls)
+    #' create_archive(commit, output_dir, fls)
     
     zipped_fl <- paste0(commit, ".zip")
     zip::zipr(zipped_fl, files = dir(current_dir, full.names = T))
@@ -66,23 +66,40 @@ create_archive <- function(commit, current_dir, archive_dir) {
     
 }
 
-archive_files <- function(commit, output_dir, archive_fls) {
+archive_output <- function(commit, output_dir, fls) {
     #' Writes archive fls out to the Current folder and archives the same files with a commit id
     #' @param commit the commit id
     #' @param output_dir the path to create the file archive
-    #' @param archive_fls a list of the files to archive
+    #' @param fls a list of the files to archive
     #' @examples
     #' commit <- 'master_98jgh8'
     #' output_dir <- '.'
-    #' archive_fls <- c('data.csv', 'data_log.log')
-    #' archive_files(commit, output_dir, archive_fls)
+    #' fls <- c('data.csv', 'data_log.log')
+    #' archive_output(commit, output_dir, fls)
     
     archive_dir <- create_archive_dir(output_dir)
     current_dir <- create_current_dir(output_dir)
     
     create_archive_str(output_dir)
-    move_to_current(archive_fls, current_dir)
+    move_to_current(fls, current_dir)
     create_archive(commit, current_dir, archive_dir)
     
+}
+
+write_output <- function(output_dir, fls) {
+  #' Writes archive fls out to the Current folder 
+  #' @param output_dir the path to create the file archive
+  #' @param fls a list of the files to archive
+  #' @examples
+  #' output_dir <- '.'
+  #' fls <- c('data.csv', 'data_log.log')
+  #' write_output(output_dir, fls)
+  
+  archive_dir <- create_archive_dir(output_dir)
+  current_dir <- create_current_dir(output_dir)
+  
+  create_archive_str(output_dir)
+  move_to_current(fls, current_dir)
+  
 }
 
