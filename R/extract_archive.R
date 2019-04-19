e47dc3b79d2d1c5d80eadc8601742a303874cbe9
holmesjoli <holmesjoli@gmail.com>
2019-04-19 14:44:59 -0500

    included archive_connection function in the two extract functions, so user would not have to call

--- R/extract_archive.R
+++ R/extract_archive.R
@@ -5,12 +5,7 @@ archive_connect <- function(output_dir, commit, fl) {
     #' @param output_dir the directory where the archive structure is
     #' @param commit the commit id
     #' @param fl the file name to extract
-    #' @returns the archive connection
-    #' @examples 
-    #' output_dir <- './tests/testthat/Data'
-    #' commit <- 'bd429f' #this will have to be changed
-    #' fl <- 'data.csv'
-    #' archive <- archive_connect(output_dir, commit, fl)
+    #' @return the archive connection
     
     archive_dir <- create_archive_dir(output_dir)
     archive_name <- grep(commit, list.files(archive_dir), value = TRUE)
@@ -20,34 +15,38 @@ archive_connect <- function(output_dir, commit, fl) {
     return(archive)
 }
 
-extract_csv <- function(archive) {
+extract_csv <- function(output_dir, commit, fl) {
     #' Extracts a csv (comma delimited) file from the archive
-    #' @param archive the archive connection 
-    #' @returns the data from the file specified
+    #' @param output_dir the directory where the archive structure is
+    #' @param commit the commit id
+    #' @param fl the file name to extract
+    #' @return the data from the file specified
     #' @examples 
     #' output_dir <- './tests/testthat/Data'
     #' commit <- 'bd429f' #this will have to be changed
     #' fl <- 'data.csv'
-    #' archive <- archive_connect(output_dir, commit, fl)
-    #' df <- extract_csv(archive)
+    #' df <- extract_csv(output_dir, commit, fl)
     
+    archive <- archive_connect(output_dir, commit, fl)
     df <- read.table(archive, header = TRUE, sep = ",")
     
     return(df)
     
 }
 
-extract_tsv <- function(archive) {
+extract_tsv <- function(output_dir, commit, fl) {
     #' Extracts a tsv (tab delimited) file from the archive
-    #' @param archive the archive connection 
-    #' @returns the data from the file specified
+    #' @param output_dir the directory where the archive structure is
+    #' @param commit the commit id
+    #' @param fl the file name to extract
+    #' @return the data from the file specified
     #' @examples 
     #' output_dir <- './tests/testthat/Data'
     #' commit <- 'bd429f' #this will have to be changed
     #' fl <- 'data.tsv'
-    #' archive <- archive_connect(output_dir, commit, fl)
-    #' df <- extract_tsv(archive)
+    #' df <- extract_tsv(output_dir, commit, fl)
     
+    archive <- archive_connect(output_dir, commit, fl)
     df <- read.table(archive, header = TRUE, sep = "\t")
     
     return(df)
