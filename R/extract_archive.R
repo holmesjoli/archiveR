b25b12aa6fd1a38230073f74a9beec98d064afb0
holmesjoli <holmesjoli@gmail.com>
2019-04-19 12:57:30 -0500

    initial commit

---
+++ R/extract_archive.R
@@ -0,0 +1,13 @@
+# Loads data from the archive
+
+extract_archive <- function(commit, fl){
+  #' Extracts the file from an archive 
+  #' @param commit the commit id
+  #' @param fl the file name to extract
+ 
+  
+  dir <- dirname(commit)
+  hash <- 
+  
+  
+}
