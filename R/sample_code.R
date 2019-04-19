387684296dbf78c55c25f6301c7a4d71eca65966
holmesjoli <holmesjoli@gmail.com>
2019-04-19 11:02:14 -0500

    test deliverables commit

--- R/sample_code.R
+++ R/sample_code.R
@@ -1,20 +1,58 @@
+library(utilsR)
 library(dataArchiveR)
+library(dplyr)
 
 output_dir <- "./tests/testthat"
-fls <- file.path(output_dir, c("data.csv"))
 
-dm <- function(fls) {
+utilsR::create_dirs(file.path(output_dir, c("Data", "Deliverables")))
+data_fls <- file.path(output_dir, "Data", c("data.csv", "data_summary.csv"))
+plot_fls <- file.path(output_dir, "Deliverables", c("hist_df.png"))
+
+## Functions
+
+dm <- function(data_fls) {
     #' Data Management
-    #' @param fls the files to archive
-    
-    
+    #' @param data_fls the files to archive
+
+
     df <- data.frame(col1 = c(1:4), col2 = c("a", "b", "c", "d"))
-    df["new_col"] <- ifelse(df["col1"] > 1, 1, 0)
-    
-    write.csv(x = df, file = fls[1])
-    
+    df$new_col <- ifelse(df$col1 > 1, 1, 0)
+
+    write.csv(x = df, file = data_fls[1])
+
+    return(df)
+}
+
+dm_sum <- function(df, data_fls){
+  #' Summarize df
+  #' @param df the dataframe
+  #' @param data_fls the names of the data files to write out
+
+  df_sum <- df %>%
+    dplyr::group_by(new_col) %>%
+    dplyr::summarize(n = dplyr::n())
+
+  write.csv(x = df, file = data_fls[2])
+
+  return(df_sum)
+}
+
+plots <- function(df, plot_fls) {
+  #' Plot Sample Data
+  #' @param df the dataframe
+  #' @param data_fls the names of the data files to write out
+
+  png(plot_fls[1])
+  hist(df$new_col)
+  dev.off()
+
 }
 
 
-dm(fls)
-archive_etl(output_dir, fls)
+## Run
+
+df <- dm(data_fls)
+df_sum <- dm_sum(df, data_fls)
+plots(df, plot_fls)
+archive_etl(output_dir, data_fls)
+archive_etl(output_dir, plot_fls)
