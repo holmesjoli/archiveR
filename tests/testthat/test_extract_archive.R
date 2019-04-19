6b519f818f442119b8cc14fb480bf5d91d2d24fd
holmesjoli <holmesjoli@gmail.com>
2019-04-19 14:31:09 -0500

    initial commit

---
+++ tests/testthat/test_extract_archive.R
@@ -0,0 +1,21 @@
+output_dir <- "./tests/testthat/Data"
+commit <- "bd429f"
+fl <- "data.csv"
+
+testthat::test_that("archive connects", {
+  
+  archive <- dataArchiveR::archive_connect(output_dir, commit, fl)
+  expect_true(class(archive)[2] == "connection")
+  
+})
+
+testthat::test_that("csv can be imported into R", {
+  
+  archive <- dataArchiveR::archive_connect(output_dir, commit, fl)
+  df_archive <- dataArchiveR::extract_csv(archive)
+  
+  df_current <- read.csv(file.path(dataArchiveR::create_current_dir(output_dir), fl))
+  
+  expect_true(all(df_archive == df_current))
+  
+})
