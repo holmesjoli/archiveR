0548f80cb40a355f6429e2439db3610bcd3a923a
holmesjoli <holmesjoli@gmail.com>
2019-04-19 11:41:27 -0500

    formatted sample code

--- R/sample_code.R
+++ R/sample_code.R
@@ -13,49 +13,46 @@ plot_fls <- file.path(output_dir, "Deliverables", c("hist_df.png", "hist_df.jpg"
 dm <- function(data_fls) {
     #' Data Management
     #' @param data_fls the files to archive
-
-
+    
+    
     df <- data.frame(col1 = c(1:4), col2 = c("a", "b", "c", "d"))
     df$new_col <- ifelse(df$col1 > 1, 1, 0)
-
+    
     write.csv(x = df, file = data_fls[1])
-
+    
     return(df)
 }
 
-dm_sum <- function(df, data_fls){
-  #' Summarize df
-  #' @param df the dataframe
-  #' @param data_fls the names of the data files to write out
-
-  df_sum <- df %>%
-    dplyr::group_by(new_col) %>%
-    dplyr::summarize(n = dplyr::n())
-
-  write.csv(x = df, file = data_fls[2])
-
-  return(df_sum)
+dm_sum <- function(df, data_fls) {
+    #' Summarize df
+    #' @param df the dataframe
+    #' @param data_fls the names of the data files to write out
+    
+    df_sum <- df %>% dplyr::group_by(new_col) %>% dplyr::summarize(n = dplyr::n())
+    
+    write.csv(x = df, file = data_fls[2])
+    
+    return(df_sum)
 }
 
 plots <- function(df, plot_fls) {
-  #' Plot Sample Data
-  #' @param df the dataframe
-  #' @param data_fls the names of the data files to write out
-
-  png(filename = plot_fls[1])
-  hist(df$new_col)
-  dev.off()
-
-  jpeg(filename = plot_fls[2])
-  hist(df$new_col)
-  dev.off()
-
-  tiff(filename = plot_fls[3])
-  hist(df$new_col)
-  dev.off()
-
+    #' Plot Sample Data
+    #' @param df the dataframe
+    #' @param data_fls the names of the data files to write out
+    
+    png(filename = plot_fls[1])
+    hist(df$new_col)
+    dev.off()
+    
+    jpeg(filename = plot_fls[2])
+    hist(df$new_col)
+    dev.off()
+    
+    tiff(filename = plot_fls[3])
+    hist(df$new_col)
+    dev.off()
+    
 }
-
 
 ## Run
 
